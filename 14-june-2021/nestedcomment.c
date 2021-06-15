64db24e7890a7c64301c84cc8a3639f4c67706e5
sukreeti90 <85863492+sukreeti90@users.noreply.github.com>
2021-06-15 14:56:03 +0530

    Update nestedcomment.c
    programcomment.c:6:9: warning: missing terminating " character
        6 |  printf("hello world2);*/
          |         ^
    programcomment.c:6:9: error: missing terminating " character
        6 |  printf("hello world2);*/
          |         ^~~~~~~~~~~~~~~~~
    programcomment.c:7:18: error: expected ‘)’ before ‘;’ token
        7 |   printf("hello");
          |                  ^
          |                  )
    programcomment.c:7:3: warning: passing argument 1 of ‘printf’ makes pointer from integer without a cast [-Wint-conversion]
        7 |   printf("hello");
          |   ^~~~~~~~~~~~~~~
          |   |
          |   int
    In file included from programcomment.c:1:
    /usr/include/stdio.h:332:43: note: expected ‘const char * restrict’ but argument is of type ‘int’
      332 | extern int printf (const char *__restrict __format, ...);
          |                    ~~~~~~~~~~~~~~~~~~~~~~~^~~~~~~~
    programcomment.c:8:1: warning: format not a string literal and no format arguments [-Wformat-security]
        8 | }
          | ^
    programcomment.c:7:19: error: expected ‘;’ before ‘}’ token
        7 |   printf("hello");

--- 14-june-2021/nestedcomment.c
+++ 14-june-2021/nestedcomment.c
@@ -1,1 +1,8 @@
-
+#include<stdio.h>
+int main()
+{  /*printf("hello world");
+    /* printf("hello world1");*/
+    printf("hello world2");*/
+     // error which i got is in the description.
+     
+}
