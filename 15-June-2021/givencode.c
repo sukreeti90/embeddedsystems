b3191a76fed956994a5d61bb2b616737235b2a30
sukreeti90 <85863492+sukreeti90@users.noreply.github.com>
2021-06-15 16:03:30 +0530

    Update givencode.c

--- 15-June-2021/givencode.c
+++ 15-June-2021/givencode.c
@@ -1,1 +1,9 @@
-
+#include<stdio.h>
+int main()
+{ int x=10, y=3;
+        {
+                int x= 100,z=10;
+                printf("%d %d",x,z);
+        }
+        printf("%d %d",x,z);
+        }
