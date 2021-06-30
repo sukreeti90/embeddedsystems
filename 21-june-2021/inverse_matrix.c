2fd6d41f8f7f0f1c9e6b914b540c723bc7a74c45
sukreeti90 <85863492+sukreeti90@users.noreply.github.com>
2021-06-30 23:25:56 +0530

    Update inverse_matrix.c

--- 21-june-2021/inverse_matrix.c
+++ 21-june-2021/inverse_matrix.c
@@ -1,31 +1,97 @@
 #include<stdio.h>
  
-int main(){
-int mat[3][3], i, j;
-float determinant = 0;
-printf("Enter elements of matrix row wise:\n");
-for(i = 0; i < 3; i++)
-for(j = 0; j < 3; j++)
-           scanf("%d", &mat[i][j]);
-printf("\nGiven matrix is:");
-for(i = 0; i < 3; i++){
-printf("\n");
-for(j = 0; j < 3; j++)
-printf("%d\t", mat[i][j]);
-}
-//finding determinant
-for(i = 0; i < 3; i++)
-determinant = determinant + (mat[0][i] * (mat[1][(i+1)%3] * mat[2][(i+2)%3] - mat[1][(i+2)%3] * mat[2][(i+1)%3]));
-printf("\n\ndeterminant: %f\n", determinant);
-printf("\nInverse of matrix is: \n");
-for(i = 0; i < 3; i++){
-for(j = 0; j < 3; j++)
-printf("%.2f\t",((mat[(j+1)%3][(i+1)%3] * mat[(j+2)%3][(i+2)%3]) - (mat[(j+1)%3][(i+2)%3] * mat[(j+2)%3][(i+1)%3]))/ determinant);
-printf("\n");
+#include<math.h>
+ 
+float detrminant(float[25][25], float);
+ 
+void cofactors(float[25][25], float);
+ 
+void trans(float[25][25], float[25][25], float);
+ 
+main() {
+ 
+	float a[25][25], n, d;
+ 
+	int i, j;
+ 
+	printf("Enter the order of the matrix:\n");
+ 
+	scanf("%f", &n);
+ 
+	printf("Enter the elements into the matrix:\n");
+ 
+	for (i = 0; i < n; i++) {
+ 
+		for (j = 0; j < n; j++) {
+ 
+			scanf("%f", &a[i][j]);
+ 
+		}
+ 
+	}
+ 
+	d = detrminant(a, n);
+ 
+	printf("\nTHE DETERMINANT IS=%2f", d);
+ 
+	if (d == 0)
+ 
+	  printf("\nMATRIX IS NOT INVERSIBLE\n"); 
+	else
+ 
+	  cofactors(a, n);
+ 
 }
  
-   return 0;
-}
+float detrminant(float a[25][25], float k) {
+ 
+	float s = 1, det = 0, b[25][25];
+ 
+	int i, j, m, n, c;
+ 
+	if (k == 1) {
+ 
+		return (a[0][0]);
+ 
+	} else {
+ 
+		det = 0;
+ 
+		for (c = 0; c < k; c++) {
+ 
+			m = 0; n = 0;
+ 
+			for (i = 0; i < k; i++) {
+ 
+				for (j = 0; j < k; j++) {
+ 
+					b[i][j] = 0;
+ 
+					if (i != 0 && j != c) {
+ 
+						b[m][n] = a[i][j];
+ 
+						if (n < (k - 2))
+ 
+						       n++; else {
+ 
+							n = 0;
+ 
+							m++;
+ 
+						}
+ 
+					}
+ 
+				}
+ 
+			}
+ 
+			det = det + s * (a[0][c] * detrminant(b, k - 1));
+ 
+			s = -1 * s;
+ 
+		}
 
 
 
