b16d76a9161df9cc17d455a16e236218f25789c7
sukreeti90 <85863492+sukreeti90@users.noreply.github.com>
2021-07-01 12:02:50 +0530

    Create interrupt_fun_pointer.c

---
+++ 24-june-2021/interrupt_fun_pointer.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+typedef struct
+{
+	char int_name[32];
+	int (*isr)();
+}isr_t;
+
+int keyboard_interrupt()
+{
+	printf("generating keyboard_interrupt..\n");
+}
+
+int mouse_interrupt()
+{
+	printf("mouse_interrupt...\n");
+}
+int rtc_interrupt()
+{
+	printf("rtc_interrupt...\n");
+}
+int i2c_interrupt()
+{
+	printf("i2c_interrupt..\n");
+}
+int usb_interrupt()
+{
+	printf("usb_interrupt..\n");
+}
+isr_t ivt[]=
+{
+	{"isr0",keyboard_interrupt},
+	{"isr1", mouse_interrupt},
+	{"isr2", rtc_interrupt},
+	{"isr3",i2c_interrupt},	
+};
+
+int main()
+{	while(1)
+{
+	(ivt[rand()%5].isr)();
+	//sleep(1);
+}
+
+}
+/*
+output:
+mouse_interrupt...
+rtc_interrupt...
+*/
